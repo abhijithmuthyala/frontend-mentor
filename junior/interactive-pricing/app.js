2dfb521f10b688d1c09420a01a2d171000caa304
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-01-26 14:50:47 +0530

    Interactive pricing component (#5)
    * Huddle landing page - Hero section
    
    * Features section
    
    * Huddle landing page - Footer
    
    * Huddle landing page - responsive
    
    * Working app
    
    * Cross browser compat
    
    * Bug fix
    
    * Improve a11y
    
    * Fix bg img and ship

---
+++ junior/interactive-pricing/app.js
@@ -0,0 +1,104 @@
+setupPriceInputElement();
+
+function setupPriceInputElement() {
+  const priceInputEl = document.querySelector(".price-input");
+  const billBtn = document.querySelector(".btn-bill");
+  const pageViewsEl = document.querySelector(".pageviews__number");
+  const priceEl = document.querySelector(".per-month__amount");
+
+  const range = {
+    min: 1,
+    max: 5,
+    step: 1,
+  };
+  const planMap = new Map([
+    [0, { pageviews: "10K", pricePerMonth: 8 }],
+    [1, { pageviews: "50K", pricePerMonth: 12 }],
+    [2, { pageviews: "100K", pricePerMonth: 16 }],
+    [3, { pageviews: "500K", pricePerMonth: 24 }],
+    [4, { pageviews: "1M", pricePerMonth: 36 }],
+  ]);
+
+  let discountPercent = 0;
+  let planData = null;
+
+  injectInputRange();
+  priceInputHandler();
+  updatePlanBtnAriaAtts();
+
+  priceInputEl.addEventListener("input", priceInputHandler);
+  billBtn.addEventListener("click", togglePlan);
+
+  function togglePlan() {
+    const yearlyPlanDiscountPercent = 25;
+
+    discountPercent = discountPercent === 0 ? yearlyPlanDiscountPercent : 0;
+
+    billBtn.classList.toggle("toggled");
+    renderPlanData();
+    updatePlanBtnAriaAtts(discountPercent);
+  }
+
+  function updatePlanBtnAriaAtts(discountPercent) {
+    billBtn.setAttribute(
+      "aria-label",
+      `Switch to ${discountPercent > 0 ? "monthly" : "yearly"} billing`
+    );
+  }
+
+  function priceInputHandler() {
+    const targetIndex = mapInputValue();
+    planData = planMap.get(targetIndex);
+
+    renderPlanData();
+    updateSliderPosition(targetIndex);
+    updatePriceInputAriaAtts(planData.pricePerMonth);
+  }
+
+  function updateSliderPosition(targetIndex) {
+    priceInputEl.style.setProperty(
+      "--left-pos",
+      `${(targetIndex / (planMap.size - 1)) * 100}%`
+    );
+    priceInputEl.style.setProperty(
+      "--transform-x",
+      `${
+        (targetIndex === 0 && "0") ||
+        (targetIndex === planMap.size - 1 && "-100%") ||
+        "-50%"
+      }`
+    );
+  }
+
+  function renderPlanData() {
+    let { pageviews, pricePerMonth } = planData;
+    pricePerMonth = ((pricePerMonth * (100 - discountPercent)) / 100).toFixed(
+      2
+    );
+
+    priceEl.textContent = `$${pricePerMonth}`;
+    pageViewsEl.textContent = pageviews;
+  }
+
+  function mapInputValue() {
+    const min = +priceInputEl.getAttribute("min");
+    const max = +priceInputEl.getAttribute("max");
+    const value = +priceInputEl.value;
+
+    const alpha = (value - min) / (max - min);
+
+    const mappedValue = range.min + alpha * (range.max - range.min);
+    const steps = Math.floor((mappedValue - range.min) / range.step);
+    return steps;
+  }
+
+  function injectInputRange() {
+    Object.entries(range).forEach(([attr, value]) =>
+      priceInputEl.setAttribute(attr, value)
+    );
+  }
+
+  function updatePriceInputAriaAtts(price) {
+    priceInputEl.setAttribute("aria-valuenow", price);
+  }
+}
