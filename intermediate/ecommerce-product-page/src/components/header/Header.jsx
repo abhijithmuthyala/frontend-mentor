49cb27c0c96a273c7e0ee13a00887dc6211d6b5c
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-08-08 10:22:34 +0530

    Design Navbar

--- intermediate/ecommerce-product-page/src/components/header/Header.jsx
+++ intermediate/ecommerce-product-page/src/components/header/Header.jsx
@@ -1,33 +1,25 @@
+import HeaderNav from "./HeaderNav";
+
 export default function Header() {
   return (
     <>
-      <header>
+      <header className="flex items-center flex-wrap gap-4 max-w-screen-xl mx-auto py-5 ">
         <img
           src="./images/logo.svg"
           alt="Sneakers logo"
           width={138}
           height={20}
+          className="max-w-full block"
         />
-        <ul>
-          <li>
-            <a href="/collections">Collections</a>
-          </li>
-          <li>
-            <a href="/men">Men</a>
-          </li>
-          <li>
-            <a href="/women">Women</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
-        <button>Cart</button>
-        <button>Avatar</button>
-        <button>Menu</button>
+        <HeaderNav />
+        <button
+          aria-label="Cart"
+          className="ml-auto min-w-[22px] h-[20px] bg-[url('./images/icon-cart.svg')]"
+        ></button>
+        <button
+          aria-label="Profile"
+          className="w-6 h-6 bg-[url('./images/image-avatar.png')] bg-cover bg-center rounded-full"
+        ></button>
       </header>
     </>
   );
--- intermediate/ecommerce-product-page/vite.config.js
+++ intermediate/ecommerce-product-page/vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   esbuild: {
     jsxInject: `import React from 'react'`,
   },
+  plugins: [
+    react({
+      include: "**/*.jsx",
+    }),
+  ],
 });
