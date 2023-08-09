8aa511b18f1ff091adb76a1d3f161015fdb73ae3
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-08-09 13:26:50 +0530

    Fix z-index issues

--- intermediate/ecommerce-product-page/src/components/header/HeaderNav.jsx
+++ intermediate/ecommerce-product-page/src/components/header/HeaderNav.jsx
@@ -16,20 +16,20 @@ export default function HeaderNav() {
     <>
       <div
         className={`absolute h-screen w-screen left-0 top-0 bg-black bg-opacity-75 ${
-          collapsed ? "opacity-0" : "opacity-100"
+          collapsed ? "opacity-0 -z-10" : "opacity-100 z-10"
         }`}
       ></div>
       <button
         aria-label={collapsed ? "Open Menu" : "Close Menu"}
         onClick={toggleNavbar}
-        className={`order-first min-w-[16px] h-[15px] ${
+        className={`z-30 order-first w-[16px] h-[15px] ${
           icons[collapsed ? "menu" : "close"]
-        }  bg-cover relative z-20`}
+        }  bg-cover static`}
       ></button>
       <nav
         className={`${
-          collapsed ? "hidden" : "block"
-        } absolute left-0 top-0 h-screen w-2/3 bg-white pt-24 px-6 flex-col gap-4`}
+          collapsed ? "hidden" : "flex"
+        } absolute z-20 left-0 top-0 h-screen w-2/3 bg-white pt-24 px-6 flex-col gap-4`}
       >
         <ul className="flex flex-col gap-5">
           <li>
