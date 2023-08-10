98bc33715ffe13e834960a3128daf2cc5e40777e
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-08-10 18:40:35 +0530

    Use strict mode

--- intermediate/ecommerce-product-page/src/main.jsx
+++ intermediate/ecommerce-product-page/src/main.jsx
@@ -2,4 +2,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
